67ea329515fbfff70d86c3a8a9c162e289d15e7d
Tamilarasan-2108 <tamilarasanbca24@outlook.com>
2023-06-02 20:24:31 +0530

    Test01
    test01

--- src/day3/program01.java
+++ src/day3/program01.java
@@ -3,7 +3,7 @@ package day3;
 public class program01 {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to testing class")
+		System.out.println("Welcome to testing class");
 
 	}
 
